53661f1681bf4d3d703b99b19be02ab466a0f9c8
Rushik Thumar <rpt@gwu.edu>
2017-11-13 21:21:53 -0500

    Create SameTree.py

---
+++ SameTree.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None or q is None:
+            return p == q
+        
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
